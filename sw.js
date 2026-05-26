77cf969cc4db34477858714790b38458cd6b397b
uptobe8 <ia.uptobe@gmail.com>
2026-05-26 03:31:32 +0200

    ggg

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='agenda-cuidados-unificado-bulma-v2';
-const FILES=["./", "index.html", "variables.html", "personas.html", "persona.html", "medicacion.html", "seguimiento.html", "informes.html", "ajustes.html", "assets/css/app.css", "assets/js/app.js", "manifest.webmanifest", "assets/icons/icon.svg"];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
